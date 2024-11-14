7418cb0cd43468516d39d6afaca3a7b4b9feb9c8
Hewad Tahiri <hewad.tahiri@ontariotechu.net>
2024-11-13 22:29:07 -0500

    Testing Scripts.ejs.

--- Public/JavaScript/Scripts.js
+++ Public/JavaScript/Scripts.js
@@ -38,14 +38,6 @@ function show(id, title, description, due_date) {
   document.getElementById("due_date").value = due_date ? due_date.split("T")[0] : "";
   document.getElementById("form").action = `/tasks/edit/${id}`;
   document.getElementById("edit").style.display = "block";
-
-  // Sets the minimum date for due_date input to today’s date.
-  const input = document.getElementById("due_date");
-  if (input) {
-    const today = new Date().toISOString().split("T")[0];
-    input.setAttribute("min", today);
-    console.log("Min date set to:", today);
-  }
 }
 
 // Hides the edit tasks section.
